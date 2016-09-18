9a298097a6fde8c86be5f05fc8160854663e99e2
Aubry Herve <hagaten_buray@yahoo.fr>
2016-09-18 09:28:47 +0200

    get all pulbic gists and do a basic format

---
+++ app/controllers/gists_controller.rb
@@ -0,0 +1,16 @@
+class GistsController < ApplicationController
+	def index
+		gists = HTTParty.get("https://api.github.com/gists/public", :verify => false).parsed_response
+		@gists = Array.new
+		gists.each do |gist|
+			formated_gist = Hash.new
+
+			formated_gist["owner"] = gist["owner"].nil? ? "anonymous" : gist["owner"]["login"]
+			formated_gist["description"] = gist["description"].nil? ? "no description" : gist["description"]
+			formated_gist["updated_at"] = gist["updated_at"]
+			formated_gist["number_of_files"] = gist["files"].nil? ? "0" : gist["files"].length
+			formated_gist["number_of_comments"] = gist["comments"].nil? ? "0" : gist["comments"]
+			@gists.push formated_gist
+		end
+	end
+end
