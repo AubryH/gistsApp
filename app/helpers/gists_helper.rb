da9d8052b804e15355777a27069b2c5f45b4e75e
Aubry Herve <hagaten_buray@yahoo.fr>
2016-09-18 13:04:40 +0200

    make a GistsHelper

--- app/helpers/gists_helper.rb
+++ app/helpers/gists_helper.rb
@@ -1,2 +1,19 @@
 module GistsHelper
+	def format_gist(gist)
+		formated_gist = Hash.new
+
+		formated_gist["owner"] = gist["owner"].nil? ? "anonymous" : gist["owner"]["login"]
+		formated_gist["description"] = gist["description"].to_s == '' ? "no description" : gist["description"]
+		formated_gist["created_at"] = _format_date(gist["created_at"])
+		formated_gist["updated_at"] = _format_date(gist["updated_at"])
+		formated_gist["number_of_files"] = gist["files"].nil? ? "0" : gist["files"].length
+		formated_gist["number_of_comments"] = gist["comments"].nil? ? "0" : gist["comments"]
+
+		return formated_gist
+	end
+
+	private
+	def _format_date(datetime_string)
+		DateTime.parse(datetime_string).strftime("%e %b %Y %T")
+	end
 end
